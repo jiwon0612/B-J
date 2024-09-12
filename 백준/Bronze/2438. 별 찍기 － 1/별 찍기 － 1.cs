66d5e9164c89cc97a98a126502f0765d75bdd539
jiwon0612 <157548104+jiwon0612@users.noreply.github.com>
2024-09-13 00:39:04 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 284 ms, Memory: 6572 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,9 @@
+int n = int.Parse(Console.ReadLine());
+            for (int a =0;a<n;a++ )
+            {
+                for (int b =0; b<=a;b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
