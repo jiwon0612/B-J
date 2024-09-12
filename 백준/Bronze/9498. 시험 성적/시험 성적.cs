7a2c8ac70c1f2145921dfbcb76bd240b5300b2a6
jiwon0612 <157548104+jiwon0612@users.noreply.github.com>
2024-09-13 00:43:50 +0900

    [Bronze V] Title: 시험 성적, Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cs
@@ -0,0 +1,26 @@
+int n = int.Parse(Console.ReadLine());
+
+            if (n>89)
+            {
+                Console.WriteLine("A");
+            }
+
+            else if (n>79)
+            {
+                Console.WriteLine("B");
+            }
+
+            else if (n > 69)
+            {
+                Console.WriteLine("C");
+            }
+
+            else if (n > 59)
+            {
+                Console.WriteLine("D");
+            }
+
+            else
+            {
+                Console.WriteLine("F");
+            }
