038217afab4e9587b69650978ef11dfb127ee0a7
jiwon0612 <157548104+jiwon0612@users.noreply.github.com>
2024-09-13 00:41:47 +0900

    [Bronze V] Title: 윤년, Time: 64 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -0,0 +1,14 @@
+int year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (year % 400 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
