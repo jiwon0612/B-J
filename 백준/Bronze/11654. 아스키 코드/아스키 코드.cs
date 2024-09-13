95976ec3d392181d197c4fd5acbf0dda08ac914a
jiwon0612 <157548104+jiwon0612@users.noreply.github.com>
2024-09-13 11:31:46 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int answer = Convert.ToInt32(input[0]);
+
+            Console.WriteLine(answer);
+        }
+
+    }
+}
