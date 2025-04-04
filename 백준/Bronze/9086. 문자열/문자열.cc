df2bb98863fd39c7685aa325da8c239d12d9c16c
jiwon0612 <157548104+jiwon0612@users.noreply.github.com>
2025-04-04 12:59:57 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		string str;
+		cin >> str;
+		cout << str.front() << str.back() << '\n';
+	}
+}
